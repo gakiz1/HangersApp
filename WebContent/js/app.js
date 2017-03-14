63e664eb6642d374f6c81f328fac75c4f51a5517
gakiz1 <ganeshvk93@gmail.com>
2017-03-14 16:28:32 +0530

    hja

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -3,13 +3,13 @@ angular.module('testModule', [ 'ngRoute'])
 .config(function($routeProvider) {
                 $routeProvider
 
-             .when('/#2', {
+             .when('/2', {
                                 templateUrl : '2.html',
-                               
+                                controller : 'insertController'
 
-                }).when('/#3', {
+                }).when('/3', {
                                 templateUrl : '3.html',
-                               
+                                controller : 'insertController'
 
                 })
 
