e7ec1b24314e768448262555570449c7c1afc6ca
gakiz1 <ganeshvk93@gmail.com>
2017-03-14 18:06:56 +0530

    q243

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,39 +1,16 @@
-/*angular.module('testModule1', [ 'ngRoute'])
-
-.config(function($routeProvider) {
-                $routeProvider
-               .when('/', {
-                                templateUrl : '2.html',
-                                controller : 'insertController'
-
-                }).when('/2', {
-                                templateUrl : '2.html',
-                                controller : 'insertController'
-
-                }).when('/3', {
-                                templateUrl : '3.html',
-                                controller : 'insertController'
-
-                })
-
-                .otherwise({
-                                redirectTo : '/'
-                });
-});*/
-
 angular.module("myApp",['ngRoute'])
 	   .config(function($routeProvider){
-	   $routeProvider
-	   .when("/",{
-	   		templateUrl : "pages/1.html"
-	   })
-	   .when("/index1.html#2",{
-	   		templateUrl : "pages/2.html"
-	   })
-	   .when("/index1.html#3",{
-	   		templateUrl : "pages/3.html"
-	   })
-	   .otherwise({
-	   		redirectTo : "/"
-	   });
+	   					$routeProvider
+	   								.when("/",{
+	   											templateUrl : "pages/1.html"
+	   									  })
+	   								.when("/2",{
+	   											templateUrl : "pages/2.html"
+	   									  })
+	   								.when("/3",{
+	   											templateUrl : "pages/3.html"
+	   									  })
+	   								.otherwise({
+	   											redirectTo : "/"
+	   									  });
 	   });
