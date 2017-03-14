ca196c6ee7937f730debaf981a4285a8402a25f5
gakiz1 <ganeshvk93@gmail.com>
2017-03-14 11:54:30 +0530

    jsakj

--- WebContent/js/contoller1.js
+++ WebContent/js/contoller1.js
@@ -1,5 +1,15 @@
 angular.module("sample",[])
 		.controller("insertController",function($scope, $http){
+		$scope.divEnabled = false;
+			$scope.addUserlog=function(){
+				var payload = {};
+				payload.uname = $scope.name;
+				payload.pass= $scope.pass;
+				console.log(payload);
+				console.log($scope.divEnabled);
+					$scope.divEnabled = true;
+				console.log($scope.divEnabled);
+			}
 			
 		});
 	
