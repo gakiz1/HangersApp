b71d04200519cc84f938ff7223d779407be3a2ed
gakiz1 <ganeshvk93@gmail.com>
2017-03-14 19:06:11 +0530

    q24

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -1,1 +1,10 @@
-angular.module("myApp",[]);
+angular.module("myApp",[])
+	   .controller("myController", function($scope, $http){
+	   $scope.divEnabled = false;
+			$scope.addUserlog=function(){
+				var payload = {};
+				payload.uname = $scope.name;
+				payload.pass= $scope.pass;
+				$scope.divEnabled = true;
+			}
+	   });
