dca04420bdf6c6cba3ad83679e16afc2536faca3
gakiz1 <ganeshvk93@gmail.com>
2017-03-15 12:06:10 +0530

    Update homeController1.js

--- WebContent/js/homeController1.js
+++ WebContent/js/homeController1.js
@@ -1,6 +1,7 @@
 angular.module("myHomeApp",[])
        .controller("myHomeController",function($scope, $location){
         $scope.addUserlog = function(){
+               console.log("testing...");
           $location.path("index1.html");
         };
        });
