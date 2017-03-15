25bc32654ddee9fd6e7cf65d245d4a5ab394bced
gakiz1 <ganeshvk93@gmail.com>
2017-03-15 12:53:42 +0530

    Update controller.js

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myApp",[]);
-	/*   .controller("myController", function($scope, $http){
+/*var myApp = angular.module("myApp",[]);
+	 .controller("myController", function($scope, $http){
 	  
 	   });*/
