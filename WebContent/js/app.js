7292ab6f1ed669eb5228bcd40b3977ecd2fdf334
gakiz1 <ganeshvk93@gmail.com>
2017-03-15 13:02:28 +0530

    Update app.js

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -2,10 +2,12 @@ angular.module("myApp",['ngRoute'])
 	   .config(function($routeProvider){
 	   					$routeProvider
 	   								.when("/2",{
-	   											templateUrl : "pages/2.html"
+	   											templateUrl : "pages/2.html",
+												controller : "myController"
 	   									  })
 	   								.when("/3",{
-	   											templateUrl : "pages/3.html"
+	   											templateUrl : "pages/3.html",
+												controller : "myController"
 	   									  })
 	   								.otherwise({
 	   											redirectTo : "/"
