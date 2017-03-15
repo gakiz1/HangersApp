ca66b9261818aad3cbe462c51aabb1cdc8301608
gakiz1 <ganeshvk93@gmail.com>
2017-03-15 12:15:18 +0530

    Update controller.js

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -1,10 +1,4 @@
 angular.module("myApp",[])
 	   .controller("myController", function($scope, $http){
-	   $scope.divEnabled = false;
-			$scope.addUserlog=function(){
-				var payload = {};
-				payload.uname = $scope.name;
-				payload.pass= $scope.pass;
-				$scope.divEnabled = true;
-			}
+	  
 	   });
