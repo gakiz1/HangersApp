c9902627c3da8e29ee04757109674daa423504bb
gakiz1 <ganeshvk93@gmail.com>
2017-03-20 16:10:23 +0530

    Update app.js

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -15,4 +15,11 @@ angular.module("myApp",['ngRoute'])
         .otherwise({
           redirectTo : "/"
         });
-       });
+       })
+       .controller('insertController',function($scope, $http){
+			$scope.name = " Somdas ";
+		})
+	   .controller('viewController', function($scope, $http){
+		   	$scope.name = " Ganesh ";
+	   });
+
