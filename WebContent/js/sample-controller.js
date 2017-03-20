aaf3e99f4699964428006a627f928119ffda209b
gakiz1 <ganeshvk93@gmail.com>
2017-03-20 15:43:20 +0530

    Update sample-controller.js

--- WebContent/js/sample-controller.js
+++ WebContent/js/sample-controller.js
@@ -1,10 +1,13 @@
-angular.module("myApp",[])
-	   .controller("sampleController", function($scope, $http){
-	   $scope.divEnabled = false;
-			$scope.addUserlog=function(){
-				var payload = {};
-				payload.uname = $scope.name;
-				payload.pass= $scope.pass;
-				$scope.divEnabled = true;
-			}
-	   });
+(function() {
+    'use strict';
+
+    angular
+        .module('myApp')
+        .factory('sampleController', sampleController);
+
+    function sampleController() { 
+    	console.log("Hello World");
+    }
+})();
+	 
+	   	
