fcb136af1211a9159cf8b418f6f207a39eed38b1
gakiz1 <ganeshvk93@gmail.com>
2017-03-20 15:08:20 +0530

    Update app.js

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,15 +1,13 @@
-angular.module("myApp",[])
-	   .config(function($routeProvider){
-		$routeProvider
-			.when("/test2",{
-				templateUrl : "pages/2.html",
-				controller: "myController"
-			})
-			.when("/test3",{
-				templateUrl : "pages/3.html",
-				controller: "myController"
-			})
-			.otherwise({
-				redirectTo : "/"
-			});
-	   });
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "pages/2.htm1"
+    })
+    .when("/test2", {
+        templateUrl : "pages/2.html"
+    })
+    .when("/test3", {
+        templateUrl : "pages/3.html"
+    })    
+});
