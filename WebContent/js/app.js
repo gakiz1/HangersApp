65dadb0fe64c25145f2686aedb7b6c15dc182d66
gakiz1 <ganeshvk93@gmail.com>
2017-03-21 12:45:21 +0530

    imjsaj

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "login.htm1"
+        templateUrl : "login.html"
         
     })
     .when("/stockIn", {
