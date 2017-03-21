85fdfa871831452ab8323ab3a67c4a35804b1c76
gakiz1 <ganeshvk93@gmail.com>
2017-03-21 11:49:43 +0530

    hcsjs

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -2,12 +2,15 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "pages/2.htm1"
+        templateUrl : "pages/login.htm1"
     })
-    .when("/test2", {
-        templateUrl : "pages/2.html"
+    .when("/stockIn", {
+        templateUrl : "pages/stockIn.html"
     })
-    .when("/test3", {
-        templateUrl : "pages/3.html"
-    })    
+    .when("/stockOut", {
+        templateUrl : "pages/stockOut.html"
+    })
+     .when("/accounts", {
+        templateUrl : "pages/accounts.html"
+    })     
 });
