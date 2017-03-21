eded8093e7bc0d78d956dcab15f88fb64de3c807
gakiz1 <ganeshvk93@gmail.com>
2017-03-21 12:33:39 +0530

    abc

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -2,16 +2,20 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "/login.htm1"
+        templateUrl : "login.htm1"
+        controller : 'sampleController'
     })
     .when("/stockIn", {
         templateUrl : "pages/stockIn.html"
+        controller : 'sampleController'
     })
     .when("/stockOut", {
         templateUrl : "pages/stockOut.html"
+        controller : 'sampleController'
     })
      .when("/accounts", {
         templateUrl : "pages/accounts.html"
+        controller : 'sampleController'
     }) 
      .otherwise({
           redirectTo : '/'
