ab8c9a78a684291986faac6b1a9999ff27941213
gakiz1 <ganeshvk93@gmail.com>
2017-03-27 15:03:39 +0530

    pojo

--- src/com/hangers/pojo/Item.java
+++ src/com/hangers/pojo/Item.java
@@ -4,49 +4,75 @@ import java.sql.Date;
 
 public class Item {
 	private String itemCode;
-	private String itemBrand;
-	private float itemPrice;
-	private String itemCategory;
-	private float sellingPrice;
-	private String itemSize;
+	private String itemType;
+	private String brand;
+	private float priceIn;
+	private float priceOut;
+	private int quantity;
+	private String size;
 	private Date dateIn;
 	private Date dateOut;
+	private String transactionId;
+	public Item() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Item(String itemCode, String itemType, String brand, float priceIn,
+			float priceOut, int quantity, String size, Date dateIn,
+			Date dateOut, String transactionId) {
+		super();
+		this.itemCode = itemCode;
+		this.itemType = itemType;
+		this.brand = brand;
+		this.priceIn = priceIn;
+		this.priceOut = priceOut;
+		this.quantity = quantity;
+		this.size = size;
+		this.dateIn = dateIn;
+		this.dateOut = dateOut;
+		this.transactionId = transactionId;
+	}
 	public String getItemCode() {
 		return itemCode;
 	}
 	public void setItemCode(String itemCode) {
 		this.itemCode = itemCode;
 	}
-	public String getItemBrand() {
-		return itemBrand;
+	public String getItemType() {
+		return itemType;
 	}
-	public void setItemBrand(String itemBrand) {
-		this.itemBrand = itemBrand;
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
 	}
-	public float getItemPrice() {
-		return itemPrice;
+	public String getBrand() {
+		return brand;
 	}
-	public void setItemPrice(float itemPrice) {
-		this.itemPrice = itemPrice;
+	public void setBrand(String brand) {
+		this.brand = brand;
 	}
-	public String getItemCategory() {
-		return itemCategory;
+	public float getPriceIn() {
+		return priceIn;
 	}
-	public void setItemCategory(String itemCategory) {
-		this.itemCategory = itemCategory;
+	public void setPriceIn(float priceIn) {
+		this.priceIn = priceIn;
 	}
-	public float getSellingPrice() {
-		return sellingPrice;
+	public float getPriceOut() {
+		return priceOut;
 	}
-	public void setSellingPrice(float sellingPrice) {
-		this.sellingPrice = sellingPrice;
+	public void setPriceOut(float priceOut) {
+		this.priceOut = priceOut;
 	}
-
-	public String getItemSize() {
-		return itemSize;
+	public int getQuantity() {
+		return quantity;
 	}
-	public void setItemSize(String itemSize) {
-		this.itemSize = itemSize;
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
 	}
 	public Date getDateIn() {
 		return dateIn;
@@ -60,29 +86,19 @@ public class Item {
 	public void setDateOut(Date dateOut) {
 		this.dateOut = dateOut;
 	}
-	public Item(String itemCode, String itemBrand, float itemPrice,
-			String itemCategory, float sellingPrice, String itemSize,
-			Date dateIn, Date dateOut) {
-		super();
-		this.itemCode = itemCode;
-		this.itemBrand = itemBrand;
-		this.itemPrice = itemPrice;
-		this.itemCategory = itemCategory;
-		this.sellingPrice = sellingPrice;
-		this.itemSize = itemSize;
-		this.dateIn = dateIn;
-		this.dateOut = dateOut;
+	public String getTransactionId() {
+		return transactionId;
 	}
-	public Item() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
 	}
 	@Override
 	public String toString() {
-		return "Item [itemCode=" + itemCode + ", itemBrand=" + itemBrand
-				+ ", itemPrice=" + itemPrice + ", itemCategory=" + itemCategory
-				+ ", sellingPrice=" + sellingPrice + ", itemSize=" + itemSize
-				+ ", dateIn=" + dateIn + ", dateOut=" + dateOut + "]";
+		return "Item [itemCode=" + itemCode + ", itemType=" + itemType
+				+ ", brand=" + brand + ", priceIn=" + priceIn + ", priceOut="
+				+ priceOut + ", quantity=" + quantity + ", size=" + size
+				+ ", dateIn=" + dateIn + ", dateOut=" + dateOut
+				+ ", transactionId=" + transactionId + "]";
 	}
 	
-}
+	}
